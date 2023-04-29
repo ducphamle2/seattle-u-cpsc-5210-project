748dd143d0eb9283a15114193c8ac09d033eba60
ducphamle2 <ducphamle212@gmail.com>
2023-04-29 12:20:26 -0700

    added mocking and parameterized test has_mission_ended method

--- test_superstartrek.py
+++ test_superstartrek.py
@@ -1,12 +1,19 @@
 from superstartrek import World
 import unittest
+from unittest import TestCase
+from unittest.mock import MagicMock
 
 
-class Testing(unittest.TestCase):
-    def test_world(self):
-        world = World()
-        print(world.remaining_time())
-        print(world.has_mission_ended())
+class Testing(TestCase):
+
+    def test_mock_has_mission_ended(self):
+        param_list = [(-1, True), (5, False)]
+        for p1, p2 in param_list:
+            with self.subTest():
+                world = World()
+                world.remaining_time = MagicMock(name='remaining_time')
+                world.remaining_time.return_value = p1
+                assert world.has_mission_ended() == p2
 
 
 if __name__ == '__main__':
