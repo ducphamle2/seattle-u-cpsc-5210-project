61d50390dca94d0a93af24f1d59028da16e9d120
ducphamle2 <ducphamle212@gmail.com>
2023-04-29 13:34:23 -0700

    removed python3 pip & old test method

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM python:3.9.16
 
-RUN apt update && apt-get -y install python3-pip
+RUN pip install parameterized
--- test_superstartrek.py
+++ test_superstartrek.py
@@ -2,18 +2,20 @@ from superstartrek import World
 import unittest
 from unittest import TestCase
 from unittest.mock import MagicMock
+from parameterized import parameterized
 
 
 class Testing(TestCase):
 
-    def test_mock_has_mission_ended(self):
-        param_list = [(-1, True), (5, False)]
-        for p1, p2 in param_list:
-            with self.subTest():
-                world = World()
-                world.remaining_time = MagicMock(name='remaining_time')
-                world.remaining_time.return_value = p1
-                self.assertEqual(world.has_mission_ended(), p2)
+    @parameterized.expand([
+        ("ended", -1, True),
+        ("not ended", 5, False),
+    ])
+    def test_mock_has_mission_ended(self, name, p1, p2):
+        world = World()
+        world.remaining_time = MagicMock(name='remaining_time')
+        world.remaining_time.return_value = p1
+        self.assertEqual(world.has_mission_ended(), p2)
 
 
 if __name__ == '__main__':
