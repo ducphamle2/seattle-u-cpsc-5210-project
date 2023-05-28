e51ecefe47f0c44c2bbe570653849ffa9809063d
sharonleo-su <sleo@seattleu.edu>
2023-05-27 19:10:23 -0700

    Add test cases for the navigation+_colculate_world_stardate() method

--- test_navigation_process.py
+++ test_navigation_process.py
@@ -15,14 +15,31 @@ class TestNavigationProcess(TestCase):
         self.world = World()
         self.ship = self.world.ship
 
+    @parameterized.expand([
+        (0, 2.5, 2.5),
+        (0.5, 5.8, 6.3),
+        (-3, 92.5, 89.5)
+    ])
+    def test_navigation_calculate_world_stardate_warp_less_than_1(self, warp_value, stardate_value, expected_result):
+        actual_result = self.game.navigation_calculate_world_stardate(warp_value, stardate_value)
+        self.assertEqual(expected_result, actual_result)
+    
+    @parameterized.expand([
+        (1, 6.2, 7.2),
+        (200, 78.5, 79.5),
+        (52.75, 0, 1)
+    ])
+    def test_navigation_calculate_world_stardate_warp_greater_than_equal_to_1(self, warp_value, stardate_value, expected_result):
+        actual_result = self.game.navigation_calculate_world_stardate(warp_value, stardate_value)
+        self.assertEqual(expected_result, actual_result)
+
+        
     @patch.object(Quadrant, 'set_value')
     def test_navigation_klingon_ship_move(self, mock):
         for klingon_ship in self.world.quadrant.klingon_ships:
-            klingon_ship.shield = 5 # value not equal to zero
+            klingon_ship.shield = 10 # value not equal to zero
         self.game.navigation_klingon_ship_move(self.world.quadrant, self.world.quadrant.klingon_ships)
-        self.assertTrue(mock.called)
-
-    
+        self.assertFalse(mock.called)
 
     
 
