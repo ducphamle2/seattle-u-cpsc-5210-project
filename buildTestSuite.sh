881b70f05409298af79765ee24801432bb3abb74
ducphamle2 <ducphamle212@gmail.com>
2023-05-29 20:07:58 -0700

    added more logs for the build test suite

--- buildTestSuite.sh
+++ buildTestSuite.sh
@@ -4,13 +4,10 @@
 email=$1
 
 # Start the container
-build_result=$(docker-compose up -d)
-build_status=true
-if echo "$build_result" | grep -q "ERROR"; then
-  build_status=false
-fi
+docker-compose up -d && build_status=true || build_status=false
 
 # Run the tests
+echo "Running the tests ..."
 test_results=$(docker-compose exec -T startrek sh -c "python -m unittest 2>&1")
 test_status=true
 
@@ -18,6 +15,8 @@ test_status=true
 if echo "$test_results" | grep -q "FAILED"; then
   test_status=false
 fi
+
+echo "Finish running the tests. Preparing to send an email"
 
 # Send the email
 echo -e "Build Status: $build_status\nTest Status: $test_status" | mail -s "Build and Test Results" $email
