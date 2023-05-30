b7e277842c9b40fffbe51dc38ab8cff72d625948
nnomier <nnomier@gmail.com>
2023-05-29 23:36:04 -0700

    fix test_world.py based on mutant testing

--- test_world.py
+++ test_world.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from world import World
 from parameterized import parameterized
+from unittest.mock import patch
 
 class TestWorld(TestCase):
 
@@ -33,3 +34,29 @@ class TestWorld(TestCase):
 
         # Assertions
         self.assertEqual(has_ended, expected_is_ended)
+
+    @parameterized.expand([
+        (0.99, 3),  # test case for r1 > 0.98
+        (0.98, 3),  # test case for r1 = 0.98 (boundary case)
+        (0.979, 2),  # test case for r1 just below 0.98 (boundary case)
+    
+        (0.97, 2),  # test case for 0.95 < r1 <= 0.98
+        (0.95, 2),  # test case for r1 = 0.95 (boundary case)
+        (0.949, 1),  # test case for r1 just below 0.95 (boundary case)
+    
+        (0.85, 1),  # test case for 0.80 < r1 <= 0.95
+        (0.80, 1),  # test case for r1 = 0.80 (boundary case)
+        (0.799, 0),  # test case for r1 just below 0.80 (boundary case)
+    
+        (0.75, 0),  # test case for r1 <= 0.80
+    ])
+    def test_remaining_klingons(self, random_return, expected_klingons):
+        total_klingons = 100
+        with patch('random.random', return_value=random_return):
+            world = World(total_klingons=total_klingons)
+            
+            # The expected remaining klingons is calculated based on the mocked random value
+            # As we have 8x8 quadrants and each quadrant will have expected_klingons because we mocked random.random()
+            expected_remaining_klingons = total_klingons - (expected_klingons * 8 * 8)
+            
+            self.assertEqual(world.remaining_klingons, expected_remaining_klingons, "Remaining klingons calculation is incorrect")
