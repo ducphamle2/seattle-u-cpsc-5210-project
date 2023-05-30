bff3de195148f0c40acfd3b66f1fbb7f67e02344
nnomier <nnomier@gmail.com>
2023-05-30 16:33:21 -0700

    apply fixes to 2 of mutmut survived mutants

--- tests/test_quadrant.py
+++ tests/test_quadrant.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 from quadrant import Quadrant
 from basic_structure import Point, QuadrantData, Position, Entity
+from parameterized import parameterized
 
 class TestQuadrant(TestCase):
 
@@ -15,3 +16,29 @@ class TestQuadrant(TestCase):
 
         # Assertions
         self.assertEqual("+K+", quadrant_str)
+
+    @parameterized.expand([
+        (0, 0),  # test case for lower boundary
+        (7, 7),  # test case for upper boundary
+        (0, 7),  # test case for mixed boundary conditions
+        (7, 0),  # test case for mixed boundary conditions
+    ])
+    def test_quadrant_initialization(self, point_x, point_y):
+        point = Point(point_x, point_y)
+        population = QuadrantData(0, 0, 0)
+        ship_position = Position(point, point)
+        quadrant = Quadrant(point, population, ship_position)
+        self.assertEqual(quadrant.name, Quadrant.quadrant_name(point.x, point.y, False))
+
+    @parameterized.expand([
+        (-1, 0),  # test case for point.x below lower boundary
+        (8, 0),  # test case for point.x above upper boundary
+        (0, -1),  # test case for point.y below lower boundary
+        (0, 8),  # test case for point.y above upper boundary
+    ])
+    def test_quadrant_initialization_out_of_bounds(self, point_x, point_y):
+        point = Point(point_x, point_y)
+        population = QuadrantData(0, 0, 0)
+        ship_position = Position(point, point)
+        with self.assertRaises(AssertionError):
+            Quadrant(point, population, ship_position)
