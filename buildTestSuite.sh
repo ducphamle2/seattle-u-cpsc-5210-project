f39e3127831a441ee5b52db593c41f7c085b339d
ducphamle2 <ducphamle212@gmail.com>
2023-05-29 19:38:43 -0700

    re-added build status

--- buildTestSuite.sh
+++ buildTestSuite.sh
@@ -3,6 +3,9 @@
 # The email address to send the status to
 email=$1
 
+# Start the container
+docker-compose up -d && build_status=true || build_status=false
+
 # Run the tests
 test_results=$(docker-compose exec -T startrek sh -c "python -m unittest 2>&1")
 test_status=true
@@ -12,6 +15,5 @@ if echo "$test_results" | grep -q "FAILED"; then
   test_status=false
 fi
 
-
 # Send the email
-echo -e "Test Status: $test_status" | mail -s "Build and Test Results" $email
+echo -e "Build Status: $build_status\nTest Status: $test_status" | mail -s "Build and Test Results" $email
