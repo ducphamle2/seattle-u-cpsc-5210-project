c96d6ca7a73e0478492694653402a756cc884cbe
zholdoshov <nur.joldoshov@gmail.com>
2023-05-30 01:19:17 -0700

    Fix test_short_range_scan.py

--- test_short_range_scan.py
+++ test_short_range_scan.py
@@ -2,31 +2,37 @@ from unittest import TestCase
 from unittest.mock import patch
 from world import World
 from superstartrek import Game
-from basic_structure import Entity
+from basic_structure import Entity, Position, Point
+from ship import Ship
+import quadrant
+from io import StringIO
+from unittest import mock
 
 class TestShortRangeScan(TestCase):
     def setUp(self):
         self.game = Game()
         self.world = World()
+        self.ship = Ship()
         
     @patch('builtins.print')
     def test_short_range_scan_docked(self, mock_print):
         # Setup
-        self.world.ship.docked = True
-        self.world.quadrant.get_value = lambda x, y: Entity.starbase if x == 2 and y == 2 else None
+        self.game.world.ship.docked = True
+        self.game.world.quadrant.get_value = lambda x, y: Entity.starbase
 
         # Call the method
         self.game.short_range_scan()
 
         # Assertions
-        self.assertTrue(self.world.ship.docked)
-        self.assertEqual(self.world.ship.shields, 0)
-        self.assertEqual(mock_print.call_count, 10)
+        self.assertTrue(self.game.world.ship.docked)
+        self.assertEqual(self.game.world.ship.shields, 0)
+        self.assertEqual(mock_print.call_count, 11)
         
     @patch('builtins.print')
     def test_short_range_scan_red(self, mock_print):
         # Setup
-        self.world.quadrant.nb_klingons = 3
+        self.game.world.quadrant.nb_klingons = -1
+        self.game.world.quadrant.get_value = lambda x, y: Entity.void
 
         # Call the method
         self.game.short_range_scan()
@@ -37,8 +43,9 @@ class TestShortRangeScan(TestCase):
     @patch('builtins.print')
     def test_short_range_scan_yellow(self, mock_print):
         # Setup
-        self.world.ship.energy = 100
-        self.world.quadrant.nb_klingons = 0
+        self.game.world.ship.energy = 100
+        self.game.world.quadrant.nb_klingons = 0
+        self.game.world.quadrant.get_value = lambda x, y: Entity.void
 
         # Call the method
         self.game.short_range_scan()
@@ -49,8 +56,9 @@ class TestShortRangeScan(TestCase):
     @patch('builtins.print')
     def test_short_range_scan_green(self, mock_print):
         # Setup
-        self.world.ship.energy = 100
-        self.world.quadrant.nb_klingons = 0
+        self.game.world.ship.energy = 100
+        self.game.world.quadrant.nb_klingons = 0
+        self.game.world.quadrant.get_value = lambda x, y: Entity.void
 
         # Call the method
         self.game.short_range_scan()
@@ -61,10 +69,11 @@ class TestShortRangeScan(TestCase):
     @patch('builtins.print')
     def test_short_range_scan_sensors_out(self, mock_print):
         # Setup
-        self.world.ship.damage_stats[1] = -1
+        self.game.world.ship.damage_stats[1] = -1
+        self.game.world.quadrant.get_value = lambda x, y: Entity.void
 
         # Call the method
         self.game.short_range_scan()
 
         # Assertions
-        mock_print.assert_called_with('---------------------------------')
+        mock_print.assert_called_with('\n*** SHORT RANGE SENSORS ARE OUT ***\n')
